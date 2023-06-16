258aa02bdf853e95ce95882ee52f36d0b63cf8ea
Juan SB <juansb.dev@gmail.com>
2023-06-16 20:26:02 -0300

    ddbb conected

--- back/src/server.js
+++ back/src/server.js
@@ -5,7 +5,7 @@ const morgan = require('morgan');
 const PORT = 3001;
 const router = require('./routes/index');
 const cors = require('cors');
-
+const { conn } = require('./DB_connection');
 
 server.use(express.json());
 
@@ -17,9 +17,18 @@ server.use(morgan('dev'));
 
 server.use('/rickandmorty', router)
 
-server.listen(PORT, ()=> {
-    console.log('SERVER RAISED IN PORT ' + PORT)
+conn.sync({force: true})
+.then(()=>{
+    console.log('ddbb conectada');
+
+    server.listen(PORT, ()=> {
+        console.log('SERVER RAISED IN PORT ' + PORT)
+    })
+
 })
+.catch(err => console.log(err.message))
+
+
 
 
 // const http = require("http");
