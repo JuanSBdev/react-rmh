76dca7ad0408679cf641298f63fae965dd39c45d
Juan SB <juansb.dev@gmail.com>
2023-10-01 23:15:59 -0300

    view form signup

--- front/src/components/signUp/SignUpForm.jsx
+++ front/src/components/signUp/SignUpForm.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
-
+import styles from './SignUpForm.module.css'
 export default function SignUpForm() {
   return (
-    <div>SignUpForm</div>
+    <div>
+        <form action="" className={styles.form}>
+            <label htmlFor="email">email</label>
+            <input type="text" />
+            <label htmlFor="email">contraseña</label>
+            <input type="password" />
+            <button className={styles.btn_reg}>Registrarme</button>
+        </form>
+
+
+    </div>
   )
 }
