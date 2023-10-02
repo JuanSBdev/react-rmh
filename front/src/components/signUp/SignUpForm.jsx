dc62e8142cda58282ff9d45041309a977ce58aa3
Juan SB <juansb.dev@gmail.com>
2023-10-01 23:33:40 -0300

    ft - form listo

--- front/src/components/signUp/SignUpForm.jsx
+++ front/src/components/signUp/SignUpForm.jsx
@@ -1,14 +1,42 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styles from './SignUpForm.module.css'
 export default function SignUpForm() {
+
+    const [userData, setUserData] = useState({
+        email:'',
+        password:'',
+    })
+
+    const handleChange = (e)=>{
+        const { name, value} = e.target;
+        setUserData ({
+            ...userData,
+            [name]: value
+        })
+        
+    }
+
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        console.log(userData)
+        // signUp(userData);
+    }
+
   return (
     <div>
-        <form action="" className={styles.form}>
+        <form action="" onSubmit={handleSubmit} className={styles.form}>
             <label htmlFor="email">email</label>
-            <input type="text" />
+            <input type="text"
+             onChange={handleChange}
+             name='email'
+             value={userData.email} />
             <label htmlFor="email">contraseña</label>
-            <input type="password" />
-            <button className={styles.btn_reg}>Registrarme</button>
+            <input type="password"
+            onChange={handleChange}
+                name='password'
+                value={userData.password}
+            />
+            <button type='submit' className={styles.btn_reg}>Registrarme</button>
         </form>
 
 
